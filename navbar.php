a6132f3405d4275beefd257684d80076cb65ac55
KarimKanji <kanjikar@arcada.fi>
2021-04-15 12:27:46 +0300

    Basic struktur

---
+++ navbar.php
@@ -0,0 +1,11 @@
+<a href="./"><img id="logo" src="logo.svg" alt="filler" /></a>
+<nav>
+    <ul>
+        <li><a href="./filler">Hem</a></li>
+        <li><a href="./filler">Annonser</a></li>
+        <li><a href="./filler">Rapport</a></li>
+    </ul>
+    <?php 
+        print(" <br><h3> Du är inloggad som: " . $_SESSION['user'] . " </h3>");
+    ?>
+</nav>
