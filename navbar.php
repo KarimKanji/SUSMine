2d4bb3384bc27345a34019c5bcacb691566c18b0
KarimKanji <kanjikar@arcada.fi>
2021-04-18 20:20:06 +0300

    Använde w3schools preset page, edited mycket

--- navbar.php
+++
@@ -1,9 +0,0 @@
-<a href="./"><img id="logo" src="logo.svg" alt="filler" /></a>
-<nav>
-    <ul>
-        <li><a href="./filler">Hem</a></li>
-        <li><a href="./filler">Annonser</a></li>
-        <li><a href="./filler">Rapport</a></li>
-    </ul>
-    
-</nav>
